0e2865dc7ab8ff1776bdd4ea5b69d575bc822ee7
Tomona Nanase <nanase@users.noreply.github.com>
2016-07-06 00:27:45 +0900

    add: ExtensionTestにテストケースを追加

--- test/UnitTest/ExtensionTest.cs
+++ test/UnitTest/ExtensionTest.cs
@@ -7,39 +7,33 @@ namespace UnitTest
     public class ExtensionTest
     {
         [Test]
-        public void ToLittleEndianInt16Test()
+        [TestCase(0, 0)]
+        [TestCase(-1, -1)]
+        [TestCase(0x3412, 0x1234)]
+        [TestCase(0x7fff, unchecked ((short)0xff7f))]
+        public void ToLittleEndianInt16Test(short input, short output)
         {
-            Assert.AreEqual((short)0, ((short)0).ToLittleEndian());
-            Assert.AreEqual((short)-1, ((short)-1).ToLittleEndian());
-
-            unchecked
-            {
-                Assert.AreEqual((short)0x1234, ((short)0x3412).ToLittleEndian());
-                Assert.AreEqual((short)0xff7f, ((short)0x7fff).ToLittleEndian());
-            }
+            Assert.AreEqual(output, input.ToLittleEndian());
         }
 
         [Test]
-        public void ToLittleEndianInt32Test()
+        [TestCase(0, 0)]
+        [TestCase(unchecked ((int)0xffffffff), -1)]
+        [TestCase(0x78563412, 0x12345678)]
+        [TestCase(0x7fffffff, unchecked((int)0xffffff7f))]
+        public void ToLittleEndianInt32Test(int input, int output)
         {
-            Assert.AreEqual(0, 0.ToLittleEndian());
-            Assert.AreEqual(-1, (int)0xffffffff.ToLittleEndian());
-
-            unchecked
-            {
-                Assert.AreEqual(0x12345678, 0x78563412.ToLittleEndian());
-                Assert.AreEqual((int)0xffffff7f, 0x7fffffff.ToLittleEndian());
-            }
+            Assert.AreEqual(output, input.ToLittleEndian());
         }
 
         [Test]
-        public void ToLittleEndianUInt32Test()
+        [TestCase(0U, 0U)]
+        [TestCase(0xffffffffU, 0xffffffffU)]
+        [TestCase(0x78563412U, 0x12345678U)]
+        [TestCase(0x7fffffffU, 0xffffff7fU)]
+        public void ToLittleEndianUInt32Test(uint input, uint output)
         {
-            Assert.AreEqual(0U, 0U.ToLittleEndian());
-            Assert.AreEqual(0xffffffffU, 0xffffffffU.ToLittleEndian());
-
-            Assert.AreEqual(0x12345678U, 0x78563412U.ToLittleEndian());
-            Assert.AreEqual(0xffffff7fU, 0x7fffffffU.ToLittleEndian());
+            Assert.AreEqual(output, input.ToLittleEndian());
         }
     }
 }
