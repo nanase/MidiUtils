2f98e9006ee7636af891d85dda1b9fcd206052ff
Tomona Nanase <nanase@users.noreply.github.com>
2016-07-10 23:57:00 +0900

    add: SequenceTestを追加

---
+++ test/UnitTest/SequenceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using MidiUtils.Sequencer;
+using NUnit.Framework;
+
+// ReSharper disable UnusedVariable
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class SequenceTest
+    {
+        [Test]
+        [TestCase("mini.mid")]
+        [TestCase("empty.mid")]
+        public void CtorTest(string filename)
+        {
+            var input_filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "midi", filename);
+            var sequence = new Sequence(input_filepath);
+        }
+    }
+}
